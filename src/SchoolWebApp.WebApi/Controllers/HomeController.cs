40a229fd9387f8aa963bba8929df9f5f7ef4ff39
Hüseyin Çınar <huseyin.cinar@hotmail.com.tr>
2020-02-13 08:39:27 +0300

    Api fix

---
+++ src/SchoolWebApp.WebApi/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SchoolWebApp.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return Ok("Welcome");
+        }
+    }
+}
